6f0787eb179f01ba3397b454659e15742a443c4b
AlexBadea <portokalh@gmail.com>
2024-01-23 14:27:28 -0500

    univariatefornow

---
+++ myfa_uni_012224_age_interaction_job.m
@@ -0,0 +1,217 @@
+%-----------------------------------------------------------------------
+% Job saved on 22-Jan-2024 16:51:42 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.factorial_design.dir = {'/Users/alex/Documents/MATLAB/ad_decode/FA_univariate_age_interaction'};
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(1).name = 'Genotype';
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(1).levels = 2;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(1).dept = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(1).variance = 1;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(1).gmsca = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(1).ancova = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(2).name = 'Sex';
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(2).levels = 2;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(2).dept = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(2).variance = 1;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(2).gmsca = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.fact(2).ancova = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(1).levels = [1
+                                                                    1];
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(1).scans = {
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02266_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02753_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03350_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S01912_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02231_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02535_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03866_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02402_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02987_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02842_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02938_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03010_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02469_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02289_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02227_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02939_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02224_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02871_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02110_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03017_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03069_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02473_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02320_fa_to_MDT.nii,1'
+                                                                   };
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(2).levels = [1
+                                                                    2];
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(2).scans = {
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S04493_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03033_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S04526_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02967_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02804_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02506_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02765_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03034_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02451_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S01470_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03889_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03321_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03890_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03308_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02926_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02812_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02817_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02373_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02720_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02410_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02524_fa_to_MDT.nii,1'
+                                                                   };
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(3).levels = [2
+                                                                    1];
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(3).scans = {
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02666_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03048_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03265_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02954_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03867_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03028_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S04491_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02654_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02670_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02491_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02690_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03293_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03225_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03847_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02813_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03391_fa_to_MDT.nii,1'
+                                                                   };
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(4).levels = [2
+                                                                    2];
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.fd.icell(4).scans = {
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02386_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02898_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02802_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03378_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02363_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02781_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02737_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02361_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02390_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02877_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02695_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03343_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02771_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02840_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02446_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03045_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03394_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02421_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S03896_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02424_fa_to_MDT.nii,1'
+                                                                   '/Users/alex/brain_data/AD_DECODE/fa_zscored_u/smoothed/S02715_fa_to_MDT.nii,1'
+                                                                   };
+%%
+matlabbatch{1}.spm.stats.factorial_design.des.fd.contrasts = 1;
+%%
+matlabbatch{1}.spm.stats.factorial_design.cov.c = [51
+                                                   69
+                                                   42.62
+                                                   36
+                                                   21
+                                                   76
+                                                   24.4
+                                                   50
+                                                   74
+                                                   36
+                                                   53
+                                                   68
+                                                   51
+                                                   25
+                                                   37
+                                                   72
+                                                   29
+                                                   52
+                                                   40
+                                                   68
+                                                   41
+                                                   52
+                                                   31
+                                                   63
+                                                   31
+                                                   44.78
+                                                   82
+                                                   52
+                                                   38
+                                                   50
+                                                   70
+                                                   47
+                                                   60.66
+                                                   50.88
+                                                   46.99
+                                                   81.65
+                                                   64.45
+                                                   40
+                                                   37
+                                                   57
+                                                   41
+                                                   70
+                                                   64
+                                                   61
+                                                   50
+                                                   83
+                                                   71.26
+                                                   67
+                                                   79.7
+                                                   42
+                                                   63
+                                                   56
+                                                   65
+                                                   38
+                                                   58
+                                                   67.86
+                                                   67.04
+                                                   20.21
+                                                   32
+                                                   76
+                                                   42
+                                                   36
+                                                   25
+                                                   65.41
+                                                   72
+                                                   35
+                                                   34
+                                                   30
+                                                   45
+                                                   44
+                                                   31
+                                                   69
+                                                   57
+                                                   51
+                                                   42
+                                                   45
+                                                   60.32
+                                                   54
+                                                   78.13
+                                                   48
+                                                   44];
+%%
+matlabbatch{1}.spm.stats.factorial_design.cov.cname = 'Age';
+matlabbatch{1}.spm.stats.factorial_design.cov.iCFI = 2;
+matlabbatch{1}.spm.stats.factorial_design.cov.iCC = 1;
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {'/Users/alex/brain_data/AD_DECODE/median_images/MDT_mask_e3.nii,1'};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
